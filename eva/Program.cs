95dfd8d0a3b8405eb19cce793987ca748626b342
Rapiz99 <41799830+Rapiz99@users.noreply.github.com>
2019-05-27 20:44:03 -0500

    Initial commit

---
+++ eva/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace eva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            billetera Billetera = new billetera();
+            Billetera.ingrese(95000);
+            Console.WriteLine($"El dinero de su cuenta es de {Billetera}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ eva/billetera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eva
+{
+    class billetera
+    {
+        public double dinero { get; set; }
+        public void ingrese(int Dinero)
+        {
+            dinero = Dinero;
+        }
+    }
+}
